160c4efd8031d3787bce490cff3669422b44600f
Jake <jakechalmers86@gmail.com>
2025-06-20 11:40:21 +0800

    link js logic to html button and render fortune to page on button click

--- Projects/FortuneTeller/index.js
+++ Projects/FortuneTeller/index.js
@@ -8,26 +8,34 @@ let fortune5 = "It would be wise to avoid the color red today.";
 let selectedFortune;
 // How to generate a random number between 1 & 5 rounded down
 let randomNumber = Math.floor(Math.random() * 5) + 1;
+let fortuneEl = document.getElementById("fortune-el")
+console.log(fortuneEl)
 
-function fortune(){
+function generateFortune(){
 if (randomNumber === 1) {
     selectedFortune = fortune1;
     console.log(selectedFortune);
+    fortuneEl.textContent = (selectedFortune)
 } 
 else if (randomNumber === 2) {
     selectedFortune = fortune2;
     console.log(selectedFortune);
+    fortuneEl.textContent = (selectedFortune)
 } 
 else if (randomNumber === 3) {
     selectedFortune = fortune3;
     console.log(selectedFortune);
+    fortuneEl.textContent = (selectedFortune)
 } 
 else if (randomNumber === 4) {
     selectedFortune = fortune4;
     console.log(selectedFortune);
+    fortuneEl.textContent = (selectedFortune)
 } 
 else if (randomNumber === 5) {
     selectedFortune = fortune5;
     console.log(selectedFortune);
+    fortuneEl.textContent = (selectedFortune)
 } 
 }
+
