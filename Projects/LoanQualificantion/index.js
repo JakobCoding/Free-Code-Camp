d989e4c71bc49b91264e9bc521da861950fcbc33
Jake <jakechalmers86@gmail.com>
2025-06-22 11:55:28 +0800

    Add Loan Qualification Project

---
+++ Projects/LoanQualificantion/index.js
@@ -0,0 +1,14 @@
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
+
+function getLoanMessage(annualIncome, creditScore) {
+  if(creditScore >= minCreditScoreForDuplex && annualIncome >= minIncomeForDuplex) {
+    return "You qualify for a duplex, condo, and car loan.";
+  }
+}
